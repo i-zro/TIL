b8c875bf06fbd55ca88ddfb37dccdb339513f483
i-zro <dek3127@gmail.com>
2022-06-03 19:16:02 +0900

    tdd setting

---
+++ 4. TDD테스트/src/test/java/com/naa0/tdd_test/TddTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naa0.tdd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
