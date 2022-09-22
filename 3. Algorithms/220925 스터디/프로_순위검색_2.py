72bb411aa8dbfbd942a7cf572b555eca2b7a4af0
i-zro <dek3127@gmail.com>
2022-09-22 14:17:51 +0900

    프로_순위검색_2차시도

---
+++ 3. Algorithms/220925 스터디/프로_순위검색_2.py
@@ -0,0 +1,23 @@
+def solution(info, query):
+    answer = []
+    
+    for q in query:
+        # query 조건 정리 리스트
+        condition = list(qq.strip() for qq in q.replace('and', '').replace('  ',' ').split(' '))
+        condition_items = set(condition[:-1]) - {'-'}
+        condition_score = int(condition[-1])
+
+        num = 0
+        
+        for i in info:
+            participant = list(ii.strip() for ii in i.split(' '))
+            participant_items = set(participant[:-1])
+            participant_score = int(participant[-1])
+            
+            if participant_score >= condition_score:
+                if len(condition_items) == len(condition_items & participant_items):
+                    num += 1
+                    
+        answer.append(num)
+        
+    return answer
